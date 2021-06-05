417fa4eea0baab080b3237d456397ac425127096
micmine <micmine4@gmail.com>
2021-06-05 22:30:10 +0200

    Add cache for configuration file

--- src/configuration.rs
+++ src/configuration.rs
@@ -1,20 +1,21 @@
+use cached::proc_macro::cached;
 use serde::{Deserialize, Serialize};
 use std::fs;
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Clone)]
 pub struct Route {
     pub method: String,
     pub path: String,
     pub resource: String,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Clone)]
 pub struct Configuration {
     routes: Vec<Route>,
 }
 
 pub fn get_routes() -> Vec<Route> {
-    load_configuration("./mockit.json").routes
+    load_configuration("./mockit.json".to_string()).routes
 }
 
 pub fn get_route<'a>(routes: &'a Vec<Route>, path: &'a str) -> Option<&'a Route> {
@@ -24,7 +25,9 @@ pub fn get_route<'a>(routes: &'a Vec<Route>, path: &'a str) -> Option<&'a Route>
         .last()
 }
 
-fn load_configuration(loaction: &str) -> Configuration {
+#[cached]
+fn load_configuration(loaction: String) -> Configuration {
+    println!("Load Configuration: {}", loaction);
     let data = fs::read_to_string(&loaction).expect("Unable to read data");
     serde_json::from_str(&data).expect("Unable to read data")
 }
